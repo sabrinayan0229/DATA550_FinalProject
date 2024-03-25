cac299c817098fe1eec59304a9df5dde798477b1
Sabrina Yan <60115658+sabrinayan0229@users.noreply.github.com>
2024-03-25 10:52:26 -0400

    Update figure.R

--- figure.R
+++ figure.R
@@ -5,9 +5,9 @@ library(dplyr)
 library(htmlwidgets)
 
 # Read in the data
-data <- read.csv("~/Downloads/Alzheimer_Disease_Healthy_Aging_Data.csv")
+data <- read.csv("~/Desktop/550/Alzheimer_Disease_Healthy_Aging_Data.csv")
 
-# Create the required figures
+# Create the required figure
 p <- ggplot(data, aes(x = as.factor(YearStart), fill = Class)) +
   geom_bar(position = "dodge") +
   labs(title = "Distribution of 'Class' Across Years",
@@ -18,8 +18,5 @@ p <- ggplot(data, aes(x = as.factor(YearStart), fill = Class)) +
         legend.position = "bottom") +
   guides(fill = guide_legend(title = "Class"))
 
-interactive_plot1 <- ggplotly(p)
-
-
-# Save the interactive plot as an HTML file
-htmlwidgets::saveWidget(interactive_plot1, "report/interactive_plot.html")
+# Save the plot as a static image file
+ggsave("~/Desktop/550/report/plot.png", plot = p, width = 8, height = 6, dpi = 300)
